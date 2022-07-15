ba201d279ee45a0529867957faf8cf3e447c4202
Dratvarik <krishtal.iwan-krishtal@yandex.ru>
2022-07-15 17:20:44 +0300

    modified:   Psiholog/__pycache__/__init__.cpython-310.pyc 	modified:   Psiholog/__pycache__/settings.cpython-310.pyc 	new file:   Psiholog/__pycache__/urls.cpython-310.pyc 	modified:   Psiholog/settings.py 	modified:   Psiholog/urls.py 	new file:   graphs/__pycache__/__init__.cpython-310.pyc 	new file:   graphs/__pycache__/admin.cpython-310.pyc 	new file:   graphs/__pycache__/apps.cpython-310.pyc 	new file:   graphs/__pycache__/models.cpython-310.pyc 	new file:   graphs/__pycache__/urls.cpython-310.pyc 	modified:   graphs/apps.py 	new file:   graphs/urls.py

--- Psiholog/settings.py
+++ Psiholog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'graphs.apps.GraphsConfig',
+
 ]
 
 MIDDLEWARE = [
@@ -103,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
@@ -117,7 +120,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "templates/static"),
+    ]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+MEDIA_URL='/media/'
--- graphs/apps.py
+++ graphs/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class GraphsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'graphs'
+    verbose_name='График'
