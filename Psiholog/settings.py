9a25e7c535dba03f5d8b9c7a8aba9ac184289220
xackermonkeay <toxa_s000@mail.ru>
2022-07-16 16:47:55 +0300

    0.2

--- Psiholog/settings.py
+++ Psiholog/settings.py
@@ -117,7 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "templates/static"),
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'templates/media')
 
 # STATICFILES_DIRS = [
 #     os.path.join(BASE_DIR, "templates/static"),
@@ -128,5 +133,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
